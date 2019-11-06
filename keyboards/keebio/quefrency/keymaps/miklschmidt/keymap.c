26cc14ff25ba457987a20f551672ced4e24e7cfa
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2019-11-06 01:26:20 +0100

    Fix lockup from rgb manipulation

--- keyboards/keebio/quefrency/keymaps/miklschmidt/keymap.c
+++ keyboards/keebio/quefrency/keymaps/miklschmidt/keymap.c
@@ -24,8 +24,6 @@ enum emoji_map {
   FLUSHED, // 😳
   MOAN, // 😩
   FIST, // ✊
-  PEACE, // ✌
-  HEART, // ❤
   UP, //👆
   DOWN, //👇
   LEFT, //👈
@@ -58,8 +56,6 @@ const uint32_t PROGMEM unicode_map[] = {
   [SCARED] = 0x1F631,
   [FLUSHED] = 0x1F633,
   [MOAN] = 0x1F629,
-  [PEACE] = 0x270C,
-  [HEART] = 0x2764,
   [FIST] = 0x270A,
   [UP] = 0x1F446,
   [DOWN] = 0x1F447,
@@ -79,7 +75,6 @@ const uint32_t PROGMEM unicode_map[] = {
 #define LY_FN MO(LAYER_FUNCTION)
 #define LY_UTIL MO(LAYER_UTIL)
 #define LY_EMOJI MO(LAYER_EMOJI)
-#define UTIL_OR_FLASKS LT(LY_UTIL, M(0))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -92,71 +87,62 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [LAYER_FUNCTION] = LAYOUT_65(
-        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,   KC_DEL,   RGB_HUI,
-        _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SLCK,  KC_PAUS,  _______,  _______,  _______,  _______,
-        _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_WH_U,  _______,
-        KC_BTN2,  _______,  _______,  KC_BTN1,  _______,  _______,  _______,  _______,  _______,  _______,  KC_WH_L,  KC_WH_D,  KC_WH_R
+        KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,   KC_DEL,   RGB_HUI,
+        KC_TRNS,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+        KC_TRNS,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_WH_U,  KC_TRNS,
+        KC_BTN2,  KC_TRNS,  KC_TRNS,  KC_BTN1,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_WH_L,  KC_WH_D,  KC_WH_R
     ),
 
     [LAYER_UTIL] = LAYOUT_65(
-        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   X(THUMBSDOWN),   X(THUMBSDOWN),   X(FIST),
-        _______,  KC_MPLY,  KC_VOLU,  KC_MSTP,  _______,  KC_I,     _______,  _______,  _______,  _______,  _______,         XP(AA, SHIFTAA),  _______,  _______,  _______,
-        KC_CAPS,  KC_MPRV,  KC_VOLD,  KC_MNXT,  _______,  KC_P,     _______,  _______,  _______,  _______,  XP(AE, SHIFTAE), XP(OE, SHIFTOE),  X(THUMBSUP),  _______,
-        _______,  _______,  KC_MUTE,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,         _______,          X(UP),  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  X(LEFT),         X(DOWN),          X(RIGHT)
+        KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_BSPC,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   X(THUMBSDOWN),   X(THUMBSDOWN),   X(FIST),
+        KC_TRNS,  KC_MPLY,  KC_VOLU,  KC_MSTP,  KC_TRNS,  KC_I,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,         XP(AA, SHIFTAA),  KC_TRNS,  KC_TRNS,  KC_TRNS,
+        KC_CAPS,  KC_MPRV,  KC_VOLD,  KC_MNXT,  KC_TRNS,  KC_P,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  XP(AE, SHIFTAE), XP(OE, SHIFTOE),  X(THUMBSUP),  KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_MUTE,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,         KC_TRNS,          X(UP),  KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  X(LEFT),         X(DOWN),          X(RIGHT)
     ),
 
     [LAYER_EMOJI] = LAYOUT_65(
-        UNICODE_MODE_WINC,   X(FIRE),     X(HEARTEYES), _______,        _______,        _______,            _______,    _______,    _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,
-        UNICODE_MODE_OSX,    X(ANGRY),    X(SMIRK),     X(SWEATSMILE),  X(JOY),         X(HEARTEYES),       _______,    _______,    _______,    _______,    _______,  _______,  _______,  _______,  _______,
-        _______,             X(TOUNGUE),  X(SCARED),    X(FLUSHED),     X(MOAN),        X(STRONK),          _______,    _______,    _______,    _______,    _______,  _______,  _______,  _______,
-        _______,             X(FIST),     X(XD),        X(PEACE),       X(THUMBSUP),    X(THUMBSDOWN),      _______,    _______,    _______,    _______,    _______,  _______,  X(UP),    _______,
-        _______,             _______,     _______,      X(KISS),        _______,        _______,            _______,    _______,    _______,    _______,    X(LEFT),  X(DOWN),  X(RIGHT)
+        UNICODE_MODE_WINC,   X(FIRE),     X(HEARTEYES), KC_TRNS,        KC_TRNS,        KC_TRNS,            KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+        UNICODE_MODE_OSX,    X(ANGRY),    X(SMIRK),     X(SWEATSMILE),  X(JOY),         X(HEARTEYES),       KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+        KC_TRNS,             X(TOUNGUE),  X(SCARED),    X(FLUSHED),     X(MOAN),        X(STRONK),          KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+        KC_TRNS,             KC_TRNS,     X(XD),        X(KISS),        X(THUMBSUP),    X(THUMBSDOWN),      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,  KC_TRNS,  X(UP),    KC_TRNS,
+        KC_TRNS,             KC_TRNS,     KC_TRNS,      KC_TRNS,        KC_TRNS,        KC_TRNS,            KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    X(LEFT),  X(DOWN),  X(RIGHT)
     )
 
 };
 
 void keyboard_post_init_user(void) {
     // Customise these values to desired behaviour
-    // debug_enable = false;
-    // debug_matrix = false;
-    // debug_keyboard=true;
-    // debug_mouse=true; s
+    debug_enable = true;
+    debug_matrix = false;
+    debug_keyboard = true;
+    debug_mouse = false;
+    rgblight_mode(14);
 }
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-    if (record->event.pressed) {
-        switch(id) {
-            case 0:
-                return MACRO( T(2), W(21), T(3), W(36), T(4), W(14), T(5), END );
-        }
-    }
-    return MACRO_NONE;
-};
 
 void eeconfig_init_user(void) {
   set_unicode_input_mode(UC_OSX);
 };
 
 // RGB Based on layers
-uint32_t layer_state_set_user(uint32_t state) {
-    switch (biton32(state)) {
-    case LAYER_FUNCTION:
-        rgblight_mode(1);
-        rgblight_setrgb(RGB_CYAN);
-        break;
-    case LAYER_UTIL:
-        rgblight_mode(1);
-        rgblight_setrgb(RGB_PINK);
-        break;
-    case LAYER_EMOJI:
-        rgblight_mode(1);
-        rgblight_setrgb(RGB_ORANGE);
-        break;
-    default: //  for any other layers, or the default layer
-        rgblight_mode(14);
-        break;
-    }
-  return state;
-}
+// uint32_t layer_state_set_user(uint32_t state) {
+//     switch (biton32(state)) {
+//     case LAYER_FUNCTION:
+//         rgblight_mode(1);
+//         rgblight_setrgb(RGB_CYAN);
+//         break;
+//     case LAYER_UTIL:
+//         rgblight_mode(1);
+//         rgblight_setrgb(RGB_PINK);
+//         break;
+//     case LAYER_EMOJI:
+//         rgblight_mode(1);
+//         rgblight_setrgb(RGB_ORANGE);
+//         break;
+//     default: //  for any other layers, or the default layer
+//         rgblight_mode(14);
+//         break;
+//     }
+//   return state;
+// }

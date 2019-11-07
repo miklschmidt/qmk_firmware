61ca8f4e1b73e6c667efb918892cf70d5f1e12c4
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2019-11-07 02:20:43 +0100

    switch grave and delete

--- keyboards/dz60/keymaps/miklschmidt/keymap.c
+++ keyboards/dz60/keymaps/miklschmidt/keymap.c
@@ -77,23 +77,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,-----------------------------------------------------------------------------------------.
-   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  | Bksp |  ` |
+   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  | Bksp | del|
    * |-----------------------------------------------------------------------------------------+
    * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \   |
    * |-----------------------------------------------------------------------------------------+
    * | Caps    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
    * |-----------------------------------------------------------------------------------------+
-   * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  |  FN |
+   * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  |     |
    * |-----------------------------------------------------------------------------------------+
    * | Ctrl |  Cmd  |  Alt  |              Space                | FN | RAlt  |  L  |  D  |  R  |
    * `-----------------------------------------------------------------------------------------'
    */
 
     [LAYER_DEFAULT] = LAYOUT_directional(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_GRAVE,
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
         KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSFT,         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
+        KC_LSFT,         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_GRAVE,
         KC_LCTL, KC_LALT, KC_LGUI, LT(LAYER_UTIL, KC_SPC), LT(LAYER_UTIL, KC_SPC), LT(LAYER_UTIL, KC_SPC), MO(LAYER_EMOJI), KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
